1b1714a7777081daeb0969664cd62ab1e354b58d
alisdhameliya <alisdhameliya24@gmail.com>
2025-05-18 21:04:46 +0530

    complete folder 6

--- 6 variable and type/_q4_type.java
+++ 6 variable and type/_q4_type.java
@@ -1,6 +1,15 @@
 public class _q4_type {
-    byte b = 4;
-    char c = 'a';
-    short s = 512;
-    
+    public static void main(String[] args) {
+        byte b = 4;
+        char c = 'z';
+        short s = 512;
+        int i = 1000;
+        float f = 3.14f;
+        double d = 99.9945;
+
+        System.out.println(((Object)((f * b) + (i % c) - (d * s))).getClass().getSimpleName());
+        // System.out.println(((f * b) + (i % c) - (d * s)));
+        char c2 = (char)(i % c);
+        System.out.println(c2);
+    }
 }
---
+++ 6 variable and type/_q5_naming.java
@@ -0,0 +1,7 @@
+public class _q5_naming {
+    public static void main(String[] args) {
+        int _ = 23;
+        int $ = 32;
+        System.out.println($);
+    }
+}
